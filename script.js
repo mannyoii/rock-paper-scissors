0472bfde894453af169ef73548e3a7b5401aba80
mannyoii <mannyoii@protonmail.com>
2023-04-27 23:59:02 +0530

    Implement the getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // r > s    s > p    p > r
+
+const signs = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+  // for (let i = 0; i < choice.length; i++) {
+
+  // }
+  const randomChoice = Math.floor(Math.random() * 3 - 0) + 0;
+  console.log(signs[randomChoice]);
+  // console.log(randomChoice)
+  return signs[randomChoice];
+}
