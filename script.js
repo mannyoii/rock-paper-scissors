193eb362c7e73938e9c618c756d66074e0d71740
mannyoii <mannyoii@protonmail.com>
2023-04-28 02:01:01 +0530

    Implement the game function and add score results

--- script.js
+++ script.js
@@ -12,21 +12,50 @@ function getComputerChoice() {
   return signs[randomChoice];
 }
 
-function playRound(playerSelection, computerSelection) {
-  const winMessage = `You Won! ${playerSelection} beats ${computerSelection}`;
-  const loseMessage = `You Lose! ${computerSelection} beats ${playerSelection}`;
-  const tieMessage = "It's a Tie!";
+function game() {
+  let userScore = 0;
+  let computerScore = 0;
+  function playRound(playerSelection, computerSelection) {
+    const winMessage = `You Won! ${playerSelection} beats ${computerSelection}`;
+    const loseMessage = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    const tieMessage = "It's a Tie!";
 
-  if (playerSelection === signs[0] && computerSelection === signs[2]) {
-    return winMessage;
-  } else if (playerSelection === signs[2] && computerSelection === signs[1]) {
-    return winMessage;
-  } else if (playerSelection === signs[1] && computerSelection === signs[0]) {
-    return winMessage;
-  } else if (playerSelection === computerSelection) {
-    return tieMessage;
-  } else return loseMessage;
+    if (playerSelection === signs[0] && computerSelection === signs[2]) {
+      userScore++;
+      console.log(userScore);
+      // return winMessage;
+    } else if (playerSelection === signs[2] && computerSelection === signs[1]) {
+      userScore++;
+      console.log(userScore);
+
+      // return winMessage;
+    } else if (playerSelection === signs[1] && computerSelection === signs[0]) {
+      userScore++;
+      console.log(userScore);
+
+      // return winMessage;
+    } else if (playerSelection === computerSelection) {
+      userScore = userScore;
+      computerScore = computerScore;
+    } else {
+      computerScore++;
+      // return loseMessage;
+    }
+  }
+
+  for (let i = 0; i < 5; i++) {
+    let userChoice = prompt("What's your choice?");
+    const playerSelection = userChoice;
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    console.log(userScore, computerScore);
+    console.log(`player: ${playerSelection}`, `computer: ${computerSelection}`);
+  }
+
+  let winner =
+    userScore > computerScore
+      ? "Player has won the game!"
+      : "Computer has won the game!";
+
+  return winner;
 }
-
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
